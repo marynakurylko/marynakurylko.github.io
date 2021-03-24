48695a33eed4b254395d1f4f27bc198f7fb879ae
Maryna Kurylko <79520532+marynakurylko@users.noreply.github.com>
2021-03-24 18:17:49 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,170 @@
+const langArr = {
+    "unit" :  {
+        "en": "Maryna Kurylko",
+        "ua": "Марина Курилко",
+    }, 
+    "about": {
+        "en": "about",
+        "ua": "про мене",
+    }, 
+    "experience": {
+        "en": "experience",
+        "ua": "досвід",
+    }, 
+    "skills": {
+        "en": "skills",
+        "ua": "навички",
+    }, 
+    "works": {
+        "en": "works",
+        "ua": "Роботи",
+    }, 
+    "contacts": {
+        "en": "contact",
+        "ua": "Контакти",
+    }, 
+    "name": {
+        "en": "Maryna Kurylko",
+        "ua": "Марина Курилко",
+    }, 
+    "portfolio": {
+        "en": "Portfolio",
+        "ua": "Портфоліо",
+    }, 
+    "aboutme": {
+        "en": "About",
+        "ua": "Про мене",
+    }, 
+    "about-about": {
+        "en": "About",
+        "ua": "Про мене",
+    }, 
+    "about-subtitle": {
+        "en": "Hi! I'm Maryna :)",
+        "ua": "Привіт! Я - Марина :)",
+    }, 
+    "about-descr": {
+        "en": "I am currently studying frontend development. Creating websites is my passion. Gaining new knowledge, skills and abilities is my lifestyle.",
+        "ua": "Наразі я вивчаю фронтенд-розробку. Створення веб-сайтів - це моє захоплення. Отримання нових знань, умінь та навичок - це мій спосіб життя.",
+    }, 
+    "about-text-web": {
+        "en": "Nothing is impossible. That's how I approach solving problems. Therefore, I do everything to make the wish a reality.",
+        "ua": "Немає нічого неможливого. Саме так я підходжу до вирішення завдань. Тому роблю все, щоб втілити бажання у реальність.",
+    }, 
+    "about-text-seo": {
+        "en": "Filling the site with content is one of the most important steps to its success. Pay very special attention to this.",
+        "ua": "Наповнення сайту контентом - це один з найважливіших кроків до його успіху. Звертайте на це особливу увагу.",
+    }, 
+    "resume-title": {
+        "en": "Experience",
+        "ua": "Досвід",
+    }, 
+    "resume-subtitle": {
+        "en": "Background",
+        "ua": "Знання",
+    }, 
+    "resume-edu": {
+        "en": "Education",
+        "ua": "Освіта",
+    }, 
+    "resume-edu-title": {
+        "en": "Ukrainan Engineering Pedagogics Academy",
+        "ua": "Українська інженерно-педагогічна академія",
+    }, 
+    "resume-edu-qual": {
+        "en": "Master's degree | Kharkiv",
+        "ua": "Магістр | Харків",
+    }, 
+    "resume-edu-pro": {
+        "en": "Vocational education. Computer Technology",
+        "ua": "Професійна освіта. Комп'ютерні технології.",
+    }, 
+    "resume-edu-ipm": {
+        "en": "Management. Intellectual property management",
+        "ua": "Менеджмент. Управління інтелектульною власністю.",
+    }, 
+    "resume-edu-udemy": {
+        "en": "Certificate | Online",
+        "ua": "Сертифікат | Онлайн",
+    }, 
+    "resume-edu-wd": {
+        "en": "Web Developer 2021 (33.5 h)",
+        "ua": "Web-разработчик 2021 (33,5 год)",
+    }, 
+    "resume-exp": {
+        "en": "Work Experience",
+        "ua": "Досвід роботи",
+    }, 
+    "resume-exp-loc": {
+        "en": "Kharkiv (2019-2020)",
+        "ua": "Харків (2019-2020)",
+    }, 
+    "resume-exp-work": {
+        "en": "(Full-time, 1 y. 2 m.)",
+        "ua": "(1 рік 2 міс)",
+    }, 
+    "skills-title": {
+        "en": "Skills",
+        "ua": "Навички",
+    }, 
+    "skills-title-hard": {
+        "en": "Hard Skills",
+        "ua": "Що використовую",
+    }, 
+    "skills-html": {
+        "en": "It forms the framework of the site and allows you to create a more optimized product structure for search engines.",
+        "ua": "Саме вона утворює каркас сайту і дозволяє створювати більш оптимізовану структуру продукту для пошукових систем.",
+    }, 
+    "skills-css": {
+        "en": "A style language that allows you to create absolutely any appearance of the site. Everything is limited only by imagination and skill!",
+        "ua": "Мова стилів, що дозволяє створювати абсолютно будь-який зовнішній вигляд сайту. Все обмежується тільки фантазією і майстерністю!",
+    }, 
+    "skills-js": {
+        "en": "This programming language allows you to animate anything: sliders, windows, hints, tabs, retrieving data from the server and much more.",
+        "ua": "Ця мова програмування дозволяє анімувати що завгодно: повзунки, вікна, підказки, вкладки, отримання даних із сервера та багато іншого",
+    }, 
+    "skills-jquery": {
+        "en": "The Jquery library will speed up development. We will not integrate it into the project without the need, but the skill of working with it is present.",
+        "ua": "Бібліотека Jquery пришвидшить розробку. Ми не будемо інтегрувати її в проєкт без необхідності.",
+    }, 
+    "portfolio-header": {
+        "en": "Works",
+        "ua": "Роботи",
+    }, 
+    "portfolio-title": {
+        "en": "Portfolio",
+        "ua": "Портфоліо",
+    }, 
+    "contact": {
+        "en": "Contacts",
+        "ua": "Контакти",
+    }, 
+    "contact-cont": {
+        "en": "Write me!",
+        "ua": "Зв'язатися",
+    }, 
+    "contact-social": {
+        "en": "On social networks:",
+        "ua": "Я у соцмережах:",
+    }, 
+    "contact-form": {
+        "en": "Or fill out the form:",
+        "ua": "Або заповніть форму:",
+    }, 
+    "contact-form-name": {
+        "en": "Name",
+        "ua": "Ім'я",
+    }, 
+    "contact-form-email": {
+        "en": "E-mail",
+        "ua": "Е-мейл",
+    }, 
+    "contact-form-text": {
+        "en": "Message",
+        "ua": "Повідомлення",
+    }, 
+    "contact-form-btn": {
+        "en": "Send",
+        "ua": "Надіслати",
+    }, 
+};
---
+++ js/script.js
@@ -0,0 +1,73 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+
+
+//document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['unit'][hash];
+    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
+/* $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+
+ */
+
+
